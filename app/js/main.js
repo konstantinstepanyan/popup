44372e1bda3feaa419e228f014ab173e3ba73710
= <=>
2020-11-29 00:20:44 +0300

    add some logic

---
+++ app/js/main.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+   class Popup {
+       constructor(data) {
+           const {targetSelector, popupSelector, displayPopup, popupTransition, autocloseTime, closeBtnSelector, closeOnMiss, closeOnKeys} = data;
+           
+           this.targetSelector = targetSelector;
+           this.popupSelector = popupSelector;
+           this.displayPopup = displayPopup;
+           this.popupTransition = popupTransition;
+           this.autocloseTime = autocloseTime;
+           this.closeBtnSelector = closeBtnSelector;
+           this.closeOnMiss = closeOnMiss;
+           this.closeOnKeys = closeOnKeys;
+           
+           this.popupTrigger = document.querySelector(`${this.targetSelector}`);
+           this.popupWindow = document.querySelector(`${this.popupSelector}`);
+           this.closeBtn = document.querySelector(`${this.closeBtnSelector}`);
+           
+           this.init();
+       }
+          
+       init(){
+           this.popupWindow.style.transition = this.popupTransition;    
+           this.popupWindow.style.display = 'none';   
+           
+           this.popupWindow.style.opacity = 0;           
+                
+           this.popupTrigger.addEventListener('click', this.appearance.bind(this));
+           this.popupTrigger.addEventListener('touchstart', this.appearance.bind(this));
+           
+           this.closeBtn.addEventListener('click', this.disappearance.bind(this));
+           this.closeBtn.addEventListener('touchstart', this.disappearance.bind(this));
+           
+           this.popupWindow.addEventListener('transitionend', ()=>{
+               if(this.popupWindow.style.display != 'none' && this.popupWindow.style.opacity == 0){
+                   this.popupWindow.style.display = 'none';
+               } 
+           });
+           
+       }
+       
+       
+       appearance(){
+           this.popupWindow.style.display = this.displayPopup;   
+           setTimeout(()=>{this.popupWindow.style.opacity = 1},1);  //найти более элегантное решение на Тостере
+           
+           if(this.autocloseTime){
+               setTimeout(()=>{
+                   this.disappearance();  
+               }, this.autocloseTime)
+           }
+       }
+       
+       
+       disappearance(){
+           this.popupWindow.style.opacity = 0;   
+           
+       }
+              
+   } 
+    
+    const popup = new Popup({
+        targetSelector: '.nav-list',
+        popupSelector: '.explore-your-music',
+        closeBtnSelector: '.rectangle',
+        displayPopup: 'block',
+        popupTransition: 'all 1s linear',
+        
+        //autocloseTime: 10000, //ms
+        closeOnMiss: true,
+        closeOnKeys: ''
+        });
+});
---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer'),
+    gcmq = require('gulp-group-css-media-queries');
+
+
+gulp.task('clean', async function () {
+    del.sync('dist')
+})
+
+gulp.task('scss', function () {
+    return gulp.src('app/scss/style.scss') //стили пишем в разных файлах и импортируем их в style.scss. А дальшне галп разберётся
+
+
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+      .pipe(gcmq())
+        .pipe(gulp.dest('app/css'))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+      .pipe(gcmq())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('css', function () {
+    return gulp.src([
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/slick-carousel/slick/slick.css',
+  ])
+        .pipe(concat('_libs.scss'))
+        .pipe(gulp.dest('app/scss'))
+        .pipe(gcmq())
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('script', function () {
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('js', function () {
+    return gulp.src([
+    'node_modules/slick-carousel/slick/slick.js'
+  ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+});
+
+gulp.task('export', function () {
+    let buildHtml = gulp.src('app/**/*.html')
+        .pipe(gulp.dest('dist'));
+
+    let BuildCss = gulp.src('app/css/**/*.css')
+        .pipe(gulp.dest('dist/css'));
+
+    let BuildJs = gulp.src('app/js/**/*.js')
+        .pipe(gulp.dest('dist/js'));
+
+    let BuildFonts = gulp.src('app/fonts/**/*.*')
+        .pipe(gulp.dest('dist/fonts'));
+
+    let BuildImg = gulp.src('app/img/**/*.*')
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+    gulp.watch('app/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('build', gulp.series('clean', 'export'))
+
+gulp.task('default', gulp.parallel('scss', 'css', 'js', 'browser-sync', 'watch'));
