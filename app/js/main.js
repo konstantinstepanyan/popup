5b3bf0c715f0200d3e4f9f899af942de4d7d28dd
= <=>
2020-11-29 23:28:06 +0300

    rewrite basic logic

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     
    class Popup {
        constructor(data) {
-           const {targetSelector, popupSelector, displayPopup, popupTransition, autocloseTime, closeBtnSelector, closeOnMiss, closeOnKeys} = data;
+           const {targetSelector, popupSelector, 
+                  displayPopup, popupHeight, popupWidth, 
+                  popupTransition, autocloseTime,
+                  closeBtnSelector, closeOnMiss, closeOnKeys} = data;
            
            this.targetSelector = targetSelector;
            this.popupSelector = popupSelector;
            this.displayPopup = displayPopup;
+           this.popupHeight = popupHeight;
+           this.popupWidth = popupWidth;
            this.popupTransition = popupTransition;
            this.autocloseTime = autocloseTime;
            this.closeBtnSelector = closeBtnSelector;
@@ -21,41 +26,29 @@ document.addEventListener('DOMContentLoaded', () => {
        }
           
        init(){
-           this.popupWindow.style.transition = this.popupTransition;    
-           this.popupWindow.style.display = 'none';   
+           this.disappearance();
            
-           this.popupWindow.style.opacity = 0;           
-                
            this.popupTrigger.addEventListener('click', this.appearance.bind(this));
            this.popupTrigger.addEventListener('touchstart', this.appearance.bind(this));
            
            this.closeBtn.addEventListener('click', this.disappearance.bind(this));
            this.closeBtn.addEventListener('touchstart', this.disappearance.bind(this));
            
-           this.popupWindow.addEventListener('transitionend', ()=>{
-               if(this.popupWindow.style.display != 'none' && this.popupWindow.style.opacity == 0){
-                   this.popupWindow.style.display = 'none';
-               } 
-           });
-           
        }
        
        
        appearance(){
-           this.popupWindow.style.display = this.displayPopup;   
-           setTimeout(()=>{this.popupWindow.style.opacity = 1},1);  //найти более элегантное решение на Тостере
-           
-           if(this.autocloseTime){
-               setTimeout(()=>{
-                   this.disappearance();  
-               }, this.autocloseTime)
-           }
+           this.popupWindow.style.height = this.popupHeight;
+           this.popupWindow.style.width = this.popupWidth;
+           this.popupWindow.style.opacity = 1;
+           this.popupWindow.style.transition = `opacity 2s ease, height 0s ease 0s`;
        }
        
        
        disappearance(){
-           this.popupWindow.style.opacity = 0;   
-           
+           this.popupWindow.style.height = 0;
+           this.popupWindow.style.opacity = 0;
+           this.popupWindow.style.transition = `opacity 2s ease, height 0s ease 2s`;
        }
               
    } 
@@ -65,8 +58,8 @@ document.addEventListener('DOMContentLoaded', () => {
         popupSelector: '.popup',
         closeBtnSelector: '.popup__close',
         displayPopup: 'flex',
-        popupTransition: 'all 1s linear',
-        
+        popupHeight: '100vh',
+        popupWidth: '100%',
         //autocloseTime: 10000, //ms
         closeOnMiss: true,
         closeOnKeys: ''
