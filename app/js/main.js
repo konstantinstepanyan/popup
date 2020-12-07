1fe1d3a3f032fbb5b52fb1a5234859602158065e
= <=>
2020-12-08 01:10:09 +0300

    add sliding, closing on key press

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
            const {targetSelector, overlaySelector, windowSelector,
                   closeBtnSelector, displayPopup, 
                   popupHeight, popupWidth, 
-                  transitionTime, transitionType,
-                  autocloseTime, closeOnMiss, closeOnKeys} = data;
+                  disappearingTime, disappearingType, 
+                  slidingTime, slidingType, slidingDir,
+                  autocloseTime,  closeOnMiss, closeOnKeys} = data;
            
            this.targetSelector = targetSelector;
            this.overlaySelector = overlaySelector;
@@ -15,8 +16,11 @@ document.addEventListener('DOMContentLoaded', () => {
            this.displayPopup = displayPopup;
            this.popupHeight = popupHeight;
            this.popupWidth = popupWidth;
-           this.transitionTime = transitionTime;
-           this.transitionType = transitionType || 'ease';
+           this.disappearingTime = disappearingTime;
+           this.disappearingType = disappearingType || 'ease';
+           this.slidingTime = slidingTime;
+           this.slidingType = slidingType || 'ease';
+           this.slidingDir = slidingDir || 'left';
            this.autocloseTime = autocloseTime;
            this.closeOnMiss = closeOnMiss;
            this.closeOnKeys = closeOnKeys;
@@ -37,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
            
            this.closeBtn.addEventListener('click', this.disappearance.bind(this));
            this.closeBtn.addEventListener('touchstart', this.disappearance.bind(this));
-           
+ 
            if(this.closeOnMiss){
                this.popupOverlay.addEventListener('click', (e)=> { 
                    if(e.target.classList.contains(`${this.overlaySelector.slice(1)}`))
@@ -52,19 +56,75 @@ document.addEventListener('DOMContentLoaded', () => {
                    }
                });
            }
+           
+           if(this.closeOnKeys){
+               const trim = this.closeOnKeys.replace(/\s+/g, ''),
+                     arr = trim.split(',');
+                
+               document.addEventListener('keydown', (e) => {
+                   arr.forEach((item) => {
+                    if(parseInt(item, 10)=== e.keyCode){
+                        this.disappearance();
+                    }
+                }); 
+            });  
+           } 
        }
        
        appearance(){
-           this.popupOverlay.style.height = this.popupHeight;
-           this.popupOverlay.style.width = this.popupWidth;
-           this.popupOverlay.style.opacity = 1;
-           this.popupOverlay.style.transition = `opacity ${this.transitionTime/1000}s ${this.transitionType}, height 0s ${this.transitionType} 0s`;
+           if(this.slidingType){
+               this.popupWindow.style.transition = `all ${this.slidingTime/1000}s ${this.slidingType}, height 0s ${this.slidingType} 0s`;
+               switch(this.slidingTime && this.slidingType && this.slidingDir){
+                   case 'left':
+                       this.popupWindow.style.left = '0px';
+                   break;
+                   case 'right':
+                       this.popupWindow.style.right = '0px';
+                   break;
+                   case 'top':
+                       this.popupWindow.style.top = '0px';
+                   break;
+                   case 'bottom':
+                       this.popupWindow.style.bottom = '0px';
+                   break;     
+               }
+           } 
+           
+               this.popupOverlay.style.height = this.popupHeight;
+               this.popupOverlay.style.width = this.popupWidth;
+               this.popupOverlay.style.opacity = 1;
+               this.popupOverlay.style.transition = `opacity ${this.disappearingTime/1000}s ${this.disappearingType}, height 0s ${this.disappearingType} 0s`;
+
+               if(this.autocloseTime){
+                   setTimeout(() => {this.disappearance(); console.log(1)}, this.autocloseTime);
+               }
+           
        }
          
        disappearance(){
-           this.popupOverlay.style.height = 0;
-           this.popupOverlay.style.opacity = 0;
-           this.popupOverlay.style.transition = `opacity ${this.transitionTime/1000}s ${this.transitionType}, height 0s ${this.transitionType} ${this.transitionTime/1000}s`;
+           if(this.slidingTime && this.slidingType && this.slidingDir){
+               this.popupWindow.style.transition = `all ${this.slidingTime/1000}s ${this.slidingType}, height 0s ${this.slidingType} ${this.slidingTime/1000}s`;
+
+               switch(this.slidingDir){
+                   case 'left':
+                       this.popupWindow.style.left = '-100%';
+                   break;
+                   case 'right':
+                       this.popupWindow.style.right = '-100%';
+                   break;
+                   case 'top':
+                       this.popupWindow.style.top = '-100%';
+                   break;
+                   case 'bottom':
+                       this.popupWindow.style.bottom = '-100%';
+                   break;     
+               }
+
+           }
+           
+               this.popupOverlay.style.height = 0;
+               this.popupOverlay.style.opacity = 0;
+               this.popupOverlay.style.transition = `opacity ${this.disappearingTime/1000}s ${this.disappearingType}, height 0s ${this.disappearingType} ${this.disappearingTime/1000}s`;
            
        }
                
@@ -78,10 +138,13 @@ document.addEventListener('DOMContentLoaded', () => {
         displayPopup: 'flex',
         popupHeight: '100vh',
         popupWidth: '100%',
-        transitionTime: 1000, //ms
-        transitionType: 'linear', //default val: ease
-        //autocloseTime: 10000, //ms
+        disappearingTime: 600, //ms transition disappearing time
+        disappearingType: 'linear', //default val: ease transition disappearing type
+        slidingTime: 500, //ms transition sliding time
+        slidingType: 'linear', //default val: ease transition sliding type
+        slidingDir: 'top', //default: left
+        //autocloseTime: 2000, //ms
         closeOnMiss: true, 
-        closeOnKeys: ''
+        closeOnKeys: '27, 67, 88' //esc 27, c - 67, x - 88
         });
 });
